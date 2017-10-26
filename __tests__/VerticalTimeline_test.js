ff780049c9cbff110d07459004da978ace002f6b
Monnot Stéphane <monnot.stephane@gmail.com>
2017-10-27 00:40:32 +0200

    Fix lint errors

--- __tests__/VerticalTimeline_test.js
+++ __tests__/VerticalTimeline_test.js
@@ -7,7 +7,9 @@ import VerticalTimeline from '../src/VerticalTimeline';
 
 describe('VerticalTimeline', function () {
   it('should have the vertical-timeline classname', function () {
-    const component = renderIntoDocument(<VerticalTimeline><div>test1</div><div>test2</div></VerticalTimeline>);
+    const component = renderIntoDocument(
+      <VerticalTimeline><div>test1</div><div>test2</div></VerticalTimeline>
+    );
     findRenderedDOMComponentWithClass(component, 'vertical-timeline');
   });
 });
