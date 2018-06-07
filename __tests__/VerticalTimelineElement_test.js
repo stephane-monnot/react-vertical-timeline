dbef2a629006da517e1c9f918cec36738b6b436e
Joey Paris <joey@leadjig.com>
2018-06-07 17:19:07 -0400

    Adds tests for `VerticalTimelineElement`'s with and without children

--- __tests__/VerticalTimelineElement_test.js
+++ __tests__/VerticalTimelineElement_test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {
   renderIntoDocument,
-  findRenderedDOMComponentWithClass
+  findRenderedDOMComponentWithClass,
+  scryRenderedDOMComponentsWithClass,
 } from 'react-dom/test-utils';
+import VerticalTimeline from '../src/VerticalTimeline';
 import VerticalTimelineElement from '../src/VerticalTimelineElement';
 
 describe('VerticalTimeline', function () {
@@ -15,4 +17,35 @@ describe('VerticalTimeline', function () {
     const component = renderIntoDocument(<VerticalTimelineElement position="right" />);
     findRenderedDOMComponentWithClass(component, 'vertical-timeline-element--right');
   });
+
+  describe('when children is empty', function () {
+    it('should have the vertical-timeline-element--no-children classname', function () {
+      const component = renderIntoDocument(
+        <VerticalTimeline>
+          <VerticalTimelineElement />
+          <VerticalTimelineElement></VerticalTimelineElement>
+          <VerticalTimelineElement>
+
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      );
+      expect( scryRenderedDOMComponentsWithClass(component, 'vertical-timeline-element--no-children').length ).toBe(3);
+    });
+
+    it('should not have the vertical-timeline-element--no-children classname', function () {
+      const component = renderIntoDocument(
+        <VerticalTimeline>
+          <VerticalTimelineElement>
+            <div>My Child</div>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement>
+            <div>My Children</div>
+            <div>My Children</div>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      );
+      expect( scryRenderedDOMComponentsWithClass(component, 'vertical-timeline-element--no-children').length ).toBe(0);
+    });
+  });
+
 });
