ca86a6d146b2255c916c417952080fa38fc8e88d
Joey Paris <joey@leadjig.com>
2018-06-07 17:27:10 -0400

    Simplifies `VerticalTimelineElement` tests; satisfies lint test

--- __tests__/VerticalTimelineElement_test.js
+++ __tests__/VerticalTimelineElement_test.js
@@ -2,9 +2,8 @@ import React from 'react';
 import {
   renderIntoDocument,
   findRenderedDOMComponentWithClass,
-  scryRenderedDOMComponentsWithClass,
+  scryRenderedDOMComponentsWithClass
 } from 'react-dom/test-utils';
-import VerticalTimeline from '../src/VerticalTimeline';
 import VerticalTimelineElement from '../src/VerticalTimelineElement';
 
 describe('VerticalTimeline', function () {
@@ -20,32 +19,17 @@ describe('VerticalTimeline', function () {
 
   describe('when children is empty', function () {
     it('should have the vertical-timeline-element--no-children classname', function () {
-      const component = renderIntoDocument(
-        <VerticalTimeline>
-          <VerticalTimelineElement />
-          <VerticalTimelineElement></VerticalTimelineElement>
-          <VerticalTimelineElement>
-
-          </VerticalTimelineElement>
-        </VerticalTimeline>
-      );
-      expect( scryRenderedDOMComponentsWithClass(component, 'vertical-timeline-element--no-children').length ).toBe(3);
+      const component = renderIntoDocument(<VerticalTimelineElement />);
+      findRenderedDOMComponentWithClass(component, 'vertical-timeline-element--no-children');
     });
 
     it('should not have the vertical-timeline-element--no-children classname', function () {
       const component = renderIntoDocument(
-        <VerticalTimeline>
-          <VerticalTimelineElement>
-            <div>My Child</div>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement>
-            <div>My Children</div>
-            <div>My Children</div>
-          </VerticalTimelineElement>
-        </VerticalTimeline>
+        <VerticalTimelineElement>
+          <div>My Child</div>
+        </VerticalTimelineElement>
       );
-      expect( scryRenderedDOMComponentsWithClass(component, 'vertical-timeline-element--no-children').length ).toBe(0);
+      expect(scryRenderedDOMComponentsWithClass(component, 'vertical-timeline-element--no-children').length).toBe(0);
     });
   });
-
 });
