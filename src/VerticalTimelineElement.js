fbaefbdc14a2133b4d526a3634846d04b09ba8f6
Stéphane Monnot <monnot.stephane@gmail.com>
2020-10-26 23:19:09 +0900

    Fix lint error

--- src/VerticalTimelineElement.js
+++ src/VerticalTimelineElement.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { InView } from 'react-intersection-observer';
