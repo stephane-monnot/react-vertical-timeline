138079b05cdb8f3247867516726b05a076826972
Stéphane Monnot <monnot.stephane@gmail.com>
2021-10-22 01:02:55 +0900

    Fix babel config

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = {
   presets: [
     ['@babel/preset-env', { loose: true, targets: { node: 10 } }],
     '@babel/preset-react',
-    '@babel/preset-typescript',
   ],
   plugins: ['@babel/plugin-syntax-dynamic-import'],
 };
