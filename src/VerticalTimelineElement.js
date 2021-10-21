09932e4216b57902e50e73dd7b4aefd814d30c35
Stéphane Monnot <monnot.stephane@gmail.com>
2021-10-21 23:10:51 +0900

    Fix lint errors

--- src/VerticalTimelineElement.js
+++ src/VerticalTimelineElement.js
@@ -126,7 +126,10 @@ VerticalTimelineElement.defaultProps = {
   position: '',
   textClassName: '',
   visible: false,
-  intersectionObserverProps: { rootMargin: '0px 0px -40px 0px', 'triggerOnce': true },
+  intersectionObserverProps: {
+    rootMargin: '0px 0px -40px 0px',
+    triggerOnce: true,
+  },
 };
 
 export default VerticalTimelineElement;
