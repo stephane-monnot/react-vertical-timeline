8c58bb6c1f0f4fcf8e456b79e923e1634f3a689c
Stéphane Monnot <monnot.stephane@gmail.com>
2021-10-22 01:01:34 +0900

    Fix build

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
+    ['@babel/preset-env', { loose: true, targets: { node: 10 } }],
     '@babel/preset-react',
+    '@babel/preset-typescript',
   ],
   plugins: ['@babel/plugin-syntax-dynamic-import'],
-  env: {
-    modules: {
-      presets: ['@babel/preset-env'],
-    },
-  },
 };
